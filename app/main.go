111dbde6fb7b0ff9f674fb78293ee796192c1b66
aoki <aoki@aoki.dev>
2022-08-05 08:55:17 +0900

    add dist req

--- app/main.go
+++ app/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/req", func(w http.ResponseWriter, r *http.Request) {
+
+		resp, err := http.Get("http://localhost:9000/info")
+		if err != nil {
+			fmt.Fprintf(w, "Error %s", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		byteArry, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(w, "Error %s", err)
+			return
+		}
+
+		fmt.Fprintf(w, "Response: %s", string(byteArry))
+	})
+
 	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
 		hostname, err := os.Hostname()
 		if err != nil {
